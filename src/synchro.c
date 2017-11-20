af0eb7dcbca3c4488b3e134b7d56e27fff3ea9a5
Gauthier Fidry <fidryg@ensipc11.imag.fr>
2017-11-20 16:42:28 +0100

    Taille ecran OK

--- src/synchro.c
+++ src/synchro.c
@@ -1,4 +1,5 @@
 #include <pthread.h>
+#include <unistd.h>
 #include <assert.h>
 #include "synchro.h"
 #include "ensitheora.h"
@@ -7,13 +8,13 @@
 bool fini;
 
 struct MoniteurTaille {
-		pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-		pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
-		int width = -1;
-		int height = -1;
+		pthread_mutex_t mutex ;
+		pthread_cond_t cond ;
+		int width ;
+		int height ;
 };
 
-struct MoniteurTaille moniteurTaille;
+struct MoniteurTaille moniteurTaille = {PTHREAD_MUTEX_INITIALIZER, PTHREAD_COND_INITIALIZER, -1, -1};
 /* les variables pour la synchro, ici */
 
 
@@ -21,8 +22,8 @@ struct MoniteurTaille moniteurTaille;
 /* l'implantation des fonctions de synchro ici */
 void envoiTailleFenetre(th_ycbcr_buffer buffer) {
 	assert( pthread_mutex_lock( &moniteurTaille.mutex) == 0);
-	moniteurTaille.width = th_ycbcr_buffer.width;
-	moniteurTaille.height = th_ycbcr_buffer.height;
+	moniteurTaille.width = buffer->width;
+	moniteurTaille.height = buffer->height;
 	assert(pthread_cond_signal(&moniteurTaille.cond)==0);
 	assert( pthread_mutex_unlock( &moniteurTaille.mutex) == 0);
 }
@@ -38,20 +39,26 @@ void attendreTailleFenetre(int *width, int *height) {
 }
 
 void signalerFenetreEtTexturePrete() {
+	sleep(2);
 }
 
 void attendreFenetreTexture() {
+	sleep(2);
 }
 
 void debutConsommerTexture() {
+	sleep(2);
 }
 
 void finConsommerTexture() {
+	sleep(2);
 }
 
 
 void debutDeposerTexture() {
+	sleep(2);
 }
 
 void finDeposerTexture() {
+	sleep(2);
 }
