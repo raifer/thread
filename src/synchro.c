ec64973c806047117db3dd629f5286db1c34ab23
Gauthier Fidry <fidryg@ensipc11.imag.fr>
2017-11-20 17:33:58 +0100

    Ajout des fonctions gerant le mutex: VIDEO SE LANCE

--- src/synchro.c
+++ src/synchro.c
@@ -13,8 +13,21 @@ struct MoniteurTaille {
 		int width ;
 		int height ;
 };
-
 struct MoniteurTaille moniteurTaille = {PTHREAD_MUTEX_INITIALIZER, PTHREAD_COND_INITIALIZER, -1, -1};
+
+struct Moniteur{
+		pthread_mutex_t mutex;
+		pthread_cond_t cond;
+};
+struct Moniteur mTexture = {PTHREAD_MUTEX_INITIALIZER, PTHREAD_COND_INITIALIZER};
+
+struct MoniteurTextures{
+		pthread_mutex_t mutex;
+		pthread_cond_t waitFree;
+		pthread_cond_t waitTexture;
+		int nbTextures;
+};
+struct MoniteurTextures moniteurTextures = {PTHREAD_MUTEX_INITIALIZER, PTHREAD_COND_INITIALIZER, PTHREAD_COND_INITIALIZER, 0};
 /* les variables pour la synchro, ici */
 
 
@@ -39,26 +52,48 @@ void attendreTailleFenetre(int *width, int *height) {
 }
 
 void signalerFenetreEtTexturePrete() {
-	sleep(2);
+	assert( pthread_mutex_lock( &mTexture.mutex) == 0);
+	assert(pthread_cond_signal(&mTexture.cond)==0);
+	assert( pthread_mutex_unlock( &mTexture.mutex) == 0);
 }
 
 void attendreFenetreTexture() {
-	sleep(2);
+	assert( pthread_mutex_lock( &mTexture.mutex) == 0);
+	assert(pthread_cond_wait(&mTexture.cond, &mTexture.mutex)==0);
+	assert( pthread_mutex_unlock( &mTexture.mutex) == 0);
 }
 
 void debutConsommerTexture() {
-	sleep(2);
+	assert( pthread_mutex_lock( &moniteurTextures.mutex) == 0);
+	while ( !moniteurTextures.nbTextures){
+		assert(pthread_cond_wait(&moniteurTextures.waitTexture, &moniteurTextures.mutex)==0);
+	}
+	assert( pthread_mutex_unlock( &moniteurTextures.mutex) == 0);
 }
 
 void finConsommerTexture() {
-	sleep(2);
+	assert( pthread_mutex_lock( &moniteurTextures.mutex) == 0);
+
+	if(moniteurTextures.nbTextures == NBTEX-1)
+		assert(pthread_cond_signal(&moniteurTextures.waitFree)==0);
+	moniteurTextures.nbTextures--;
+	assert( pthread_mutex_unlock( &moniteurTextures.mutex) == 0);
 }
 
 
 void debutDeposerTexture() {
-	sleep(2);
+	assert( pthread_mutex_lock( &moniteurTextures.mutex) == 0);
+	while (moniteurTextures.nbTextures == NBTEX-1) {
+		assert(pthread_cond_wait(&moniteurTextures.waitFree, &moniteurTextures.mutex)==0);
+	}
+	assert( pthread_mutex_unlock( &moniteurTextures.mutex) == 0);
 }
 
 void finDeposerTexture() {
-	sleep(2);
+	assert( pthread_mutex_lock( &moniteurTextures.mutex) == 0);
+	if(!moniteurTextures.nbTextures) {
+		assert(pthread_cond_signal(&moniteurTextures.waitTexture)==0);
+	}
+	moniteurTextures.nbTextures++;
+	assert( pthread_mutex_unlock( &moniteurTextures.mutex) == 0);
 }
